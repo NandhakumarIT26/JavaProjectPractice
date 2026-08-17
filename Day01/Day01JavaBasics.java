7f61faf95cf3a65d9f9aaf9fddc665706083179d
mohanrajvijayan2410 <mohanraj@auist.net>
2026-08-17 21:21:13 +0530

    day01 checkout

---
+++ Day01/Day01JavaBasics.java
@@ -0,0 +1,51 @@
+class Day01JavaBasics{
+ public static void main(String[] args){
+    // String name = "Nandha Kumar";
+    // int age = 21;
+    // double salary = 51035;
+    // String city = "Chennai";
+    // boolean isEmployed = true;
+
+    // System.out.println(name);
+    // System.out.println(age);
+    // System.out.println(salary);
+    // System.out.println(city);
+    // System.out.println(isEmployed);
+
+    // if(age >=18){
+    //     System.out.println("Eligible to work");
+    // }else{
+    //  System.out.println("Not eligible to work");
+    // }
+
+    // if(salary>=40000){
+    //     System.out.println("Good Salary");
+    // }else{
+    //     System.out.println("Keep Growing");
+    // }
+
+
+    // if(age>=18 && isEmployed==true){
+    //     System.out.println("Working Adult");
+    // }else{
+    //     System.out.println("It's Okay");
+    // }
+    // }
+
+
+    int marks = 85;
+    if(marks>=90 && marks<=100){
+        System.out.println("A");
+    }else if(marks>=75 && marks<=89){
+        System.out.println("B");
+    }else if(marks>=60 && marks<=74){
+        System.out.println("C");
+    }else if(marks>=40 && marks<=59){
+        System.out.println("D");
+    }else{
+        System.out.println("Fail");
+    }
+    }
+
+
+}
